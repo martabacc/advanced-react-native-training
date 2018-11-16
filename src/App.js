d71ff51c29ea7720d4610fd80a886299a5b8f3b8
Rona ¯\_(ツ)_/¯ <deasnataly@gmail.com>
2018-11-16 15:07:59 +0700

    Add provider

--- src/App.js
+++ src/App.js
@@ -6,9 +6,21 @@ import NavigationReact from './navigation/NavigationReact';
 
 type Props = {};
 type State = {};
+
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'https://us1.prisma.sh/advanced-react-training-api/money-trackr/dev',
+});
+
 class App extends React.Component<Props, State> {
   render() {
-    return <NavigationReact />;
+    return (
+      <ApolloProvider client={client}>
+        <NavigationReact />
+      </ApolloProvider>
+    );
   }
 }
 
